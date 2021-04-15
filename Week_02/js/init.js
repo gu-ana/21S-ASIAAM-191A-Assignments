fa66ed93bb2f1f2a570abe971a4422c10780c8f2
gu-ana <52898059+gu-ana@users.noreply.github.com>
2021-04-14 23:50:52 -0700

    cri

--- Week_02/js/init.js
+++ Week_02/js/init.js
@@ -1,5 +1,5 @@
 // JavaScript const variable declaration
-const map = L.map('map').setView([34.0709, -118.444], 15);
+const map = L.map('map').setView([0,0], 2);
 
 // Leaflet tile layer, i.e. the base map
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
@@ -7,6 +7,18 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 }).addTo(map);
 
 //JavaScript let variable declaration to create a marker
-let marker = L.marker([34.0709, -118.444]).addTo(map)
-		.bindPopup('Math Sciences 4328 aka the Technology Sandbox<br> is the lab where I work in ')
+let marker1 = L.marker([35.689487, 139.691711]).addTo(map)
+		.bindPopup('Weebing out in Tokyo')
 		.openPopup();
+
+let marker2 = L.marker([21.306944, -157.858337]).addTo(map)
+.bindPopup('Sipping Pina Coladas in Hawaii')
+.openPopup();
+
+let marker3 = L.marker([47.6062, -122.3321]).addTo(map)
+.bindPopup('Potential city I want to work in')
+.openPopup();
+
+let marker4 = L.marker([41.8781, -87.6298]).addTo(map)
+.bindPopup('Potential city I want to work in')
+.openPopup();
